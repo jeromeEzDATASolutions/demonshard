89db4530cc744bc87a0d1ea0e476113d2d9a0a7e
15K <jbraconnier@gmail.com>
2025-04-16 19:18:28 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Damien Ciabrini
+ * This file is part of ngdevkit
+ *
+ * ngdevkit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ngdevkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ngdevkit.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional resources for sprites
+ * https://wiki.neogeodev.org/index.php?title=Sprites
+ */
+#include <ngdevkit/neogeo.h>
+#include <ngdevkit/ng-fix.h>
+#include <ngdevkit/ng-video.h>
+#include <stdio.h>
+
+#define TEST 1
+
+int main(void) {
+
+    char str[10];
+
+    ng_cls();
+
+    for(;;) {
+
+        ng_wait_vblank();
+    }
+
+    return 0;
+}
---
+++ tiled.php
@@ -0,0 +1,72 @@
+<?php
+$file = "map.tmx";
+
+if (file_exists($file)) {
+    $xml = simplexml_load_file($file);
+    $json = json_encode($xml);
+    $array = json_decode($json, TRUE);
+
+    // --- Create array for palettes
+    $cpt=0;
+    $tab = [];
+    foreach( $array["tileset"] as $tileset ){
+        if ( $cpt>0){
+            $tab[] = "{" . $tileset["@attributes"]["firstgid"] . ",".$cpt."}";
+        }
+        $cpt++;
+    }
+    echo "int palettes_offset[".count($tab)."][2] = {";
+    echo implode(",", $tab);
+    echo "};\n\n";
+
+    foreach ($array["layer"] as $key => $value) {
+        if ( $value["@attributes"]["name"] != "test" ){
+
+            if ( $value["@attributes"]["name"] == "decor"){
+
+                $nombre_tiles_width = 60;
+                if ( isset($value["properties"]["property"][1]["@attributes"]["name"]) && $value["properties"]["property"][1]["@attributes"]["name"] == "width" ) {
+                    $nombre_tiles_width = $value["properties"]["property"][1]["@attributes"]["value"];
+                }
+                $nombre_tiles_height = 45;
+                if ( isset($value["properties"]["property"][0]["@attributes"]["name"]) && $value["properties"]["property"][0]["@attributes"]["name"] == "height" ) {
+                    $nombre_tiles_height = $value["properties"]["property"][0]["@attributes"]["value"];
+                }
+
+                $data = $value["data"];
+                echo "const u16 tmx_" . $value["@attributes"]["name"] . "[".$nombre_tiles_height."][".$nombre_tiles_width."] = {\n";
+                $ligne_cpt = 0;
+                
+                $cpt = 0;
+                foreach (explode("\n", $data) as $ligne) {
+
+                    if (!empty($ligne)) {
+
+                        $cpt++;
+                        if ( $cpt <= $nombre_tiles_height ){
+
+                            $ligne_cpt++;
+                            $ligne = rtrim($ligne, ",");
+
+                            $tab_ligne = explode(",", $ligne);
+                            $tmp_array = [];
+                            for($i=0;$i<$nombre_tiles_width;$i++){
+                                $tmp_array[$i] = $tab_ligne[$i];
+                            }
+
+                            $display = true;
+                            if ($value["@attributes"]["name"] == "herbe" && $ligne_cpt < 14) {
+                                //$display = false;
+                            }
+
+                            if ($display) {
+                                echo "\t{" . implode(",", $tmp_array) . "},\n";
+                            }
+                        }
+                    }
+                }
+                echo "};\n";
+            }
+        }
+    }
+}
