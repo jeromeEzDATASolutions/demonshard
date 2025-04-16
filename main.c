c1571cee3aeeae13954db1993799454435a1398b
15K <jbraconnier@gmail.com>
2025-04-16 22:18:21 +0200

    Create scrolling multidirectionel

--- main.c
+++ main.c
@@ -23,15 +23,58 @@
 #include <ngdevkit/ng-video.h>
 #include <stdio.h>
 
-#define TEST 1
+#define CROM_TILE_OFFSET 256
+#define SPRITES_ALLOCATED 1
+
+#include "tile_layers.c"
+
+#include "sprite.h"
+#include "palette.c"
+#include "sprite.c"
 
 int main(void) {
 
     char str[10];
 
     ng_cls();
+    init_palette();
 
-    for(;;) {
+    sprite_init_tmx(0);
+    sprite_init(0);
+
+    for (;;)
+    {
+        // Get controles from joystick 1
+        u8 u = (bios_p1current & CNT_UP);
+        u8 d = (bios_p1current & CNT_DOWN);
+        u8 l = (bios_p1current & CNT_LEFT);
+        u8 r = (bios_p1current & CNT_RIGHT);
+        u8 b1 = (bios_p1current & 16);
+        u8 b2 = (bios_p1current & 32);
+
+        if (bios_p1current & CNT_UP)
+        {
+            sprites[0].y -= 1;
+            sprite_update(0);
+        }
+        else if (bios_p1current & CNT_DOWN)
+        {
+            sprites[0].y += 1;
+            sprite_update_y(0);
+        }
+        else if (bios_p1current & CNT_LEFT)
+        {
+            sprites[0].x += 1;
+            sprite_update_x(0);
+        }
+        else if (bios_p1current & CNT_RIGHT)
+        {
+            sprites[0].x -= 1;
+            sprite_update_x(0);
+        }
+
+        snprintf(str, 10, "T : %3d", sprites[0].tmx[0][0]);
+        ng_text(2, 3, 0, str);
 
         ng_wait_vblank();
     }
